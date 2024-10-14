6cdd22850c0774a5e5829f2a74a2d96a1cd45014
Chris Rock <chrispebble@gmail.com>
2024-10-13 21:27:28 -0500

    small changes to main

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 // Lab 2: LTE: Self-Driving Car - Networking
 
 //Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow(){
-    //Create the browswer window
+function createWindow() {
+    //Create the browser window
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 640,
@@ -31,17 +31,17 @@ function createWindow(){
 app.whenReady().then(() => {
     createWindow()
 
-    app.on('activate', function() {
+    app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length===0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
 // Quit when all windows are closed, except on macOS.  There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitely with Cmd+Q
-app.on('window-all-closed', function(){
+app.on('window-all-closed', function () {
     // if (process.platform !== 'darwin') app.quit()
     app.quit()
 })
